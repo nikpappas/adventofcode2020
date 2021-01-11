d090da87902cdef30ea8b722f29dc4ddb40554f2
Nikolaos Pappas <N.Pappas@elsevier.com>
2021-01-11 10:44:31 +0000

    DAY -- 18

--- src/day18OperationOrder.go
+++ src/day18OperationOrder.go
@@ -12,27 +12,12 @@ func day18() {
 	lines := files.ReadLines("inputs/day18.txt")
 	res := day18sol1(lines)
 	fmt.Println(res)
+	res64 := day18sol2(lines)
+	fmt.Println("day2", res64)
 
 }
 
-func day18sol2(lines []string) int {
-	sum := 0
-	for _, line := range lines {
-		fmt.Println("======================", line)
-		line = strings.ReplaceAll(line, " ", "")
-		fmt.Println("cleanline", line)
-
-		// line := addBrackets(line)
-		res := calcBrackets(line)
-		val, _ := strconv.Atoi(res)
-		sum += val
-		fmt.Println("======================", res)
-		fmt.Println(sum)
-	}
-	return sum
-
-}
-
+// Day 1 __vv
 func day18sol1(lines []string) int {
 	sum := 0
 	for _, line := range lines {
@@ -45,126 +30,6 @@ func day18sol1(lines []string) int {
 	}
 	return sum
 
-}
-
-// func addBrackets(line string) string {
-// 	firstPlus := indexOfFirst(line, '+')
-// 	left, right, isBracketed := getOperands(line, firstPlus)
-
-// 	return line
-
-// }
-
-// func getOperands(line string, index int) (string, string, bool) {
-
-// }
-func calcBrackets(line string) string {
-	latestOpeningBracket := indexOfLast(line, '(')
-	for latestOpeningBracket != -1 {
-		fmt.Println(line)
-		offset := indexOfFirst(line[latestOpeningBracket:], ')')
-		firstClosing := latestOpeningBracket + offset
-
-		fmt.Println("->", line[latestOpeningBracket+1:firstClosing])
-		res := calcPrecedenceLine(line[latestOpeningBracket+1 : firstClosing])
-		line = line[:latestOpeningBracket] + res + line[firstClosing+1:]
-		fmt.Println("newline", line)
-		latestOpeningBracket = indexOfLast(line, '(')
-	}
-	return calcPrecedenceLine(line)
-
-}
-
-func indexOfLast(line string, cQuery rune) int {
-	latestOpeningBracket := -1
-	for i, c := range line {
-		if c == cQuery {
-			latestOpeningBracket = i
-		}
-	}
-	return latestOpeningBracket
-}
-func indexOfFirst(line string, cQuery rune) int {
-	for i, c := range line {
-		if c == cQuery {
-			return i
-		}
-	}
-	return -1
-}
-func indexOfFirstOp(line string) int {
-	firstPlus := indexOfFirst(line, '+')
-	firstMult := indexOfFirst(line, '*')
-	if firstPlus == -1 && firstMult == -1 {
-		return -1
-	} else if firstPlus >= 0 && firstMult >= 0 {
-		if firstPlus < firstMult {
-			return firstPlus
-		} else {
-			return firstMult
-		}
-	} else if firstPlus > 0 {
-		return firstPlus
-	} else {
-		return firstMult
-	}
-
-}
-func calcPrecedenceLine(line string) string {
-	fmt.Println("line", line)
-	firstPlus := indexOfFirst(line, '+')
-	firstMult := indexOfFirst(line, '*')
-	if firstPlus == -1 && firstMult == -1 {
-		return line
-	}
-	if firstPlus >= 0 && firstMult == -1 {
-		fmt.Println("only +")
-		sum := 0
-		toks := strings.Split(line, "+")
-		for _, tok := range toks {
-			val, _ := strconv.Atoi(tok)
-			sum += val
-		}
-		return fmt.Sprint(sum)
-	}
-	if firstMult >= 0 && firstPlus == -1 {
-		fmt.Println("only *")
-		prod := 1
-		toks := strings.Split(line, "*")
-		for _, tok := range toks {
-			val, _ := strconv.Atoi(tok)
-			prod *= val
-		}
-		return fmt.Sprint(prod)
-	}
-
-	for firstPlus >= 0 {
-		if firstPlus < firstMult {
-			fmt.Println("firstPlus", firstPlus)
-			left, _ := strconv.Atoi(line[:firstPlus])
-			fmt.Println("linetofindnextop", line[firstPlus+1:])
-			nextOperation := firstPlus + indexOfFirstOp(line[firstPlus+1:]) + 1
-			rightToParse := line[firstPlus+1 : nextOperation]
-			fmt.Println("nextOperation", nextOperation)
-			fmt.Println("rightToParse", rightToParse)
-			right, _ := strconv.Atoi(rightToParse)
-			fmt.Println(left, right)
-			line = calcPrecedenceLine(fmt.Sprint(left+right) + line[nextOperation:])
-			firstPlus = indexOfFirst(line, '+')
-		} else {
-
-		}
-	}
-	for firstMult >= 0 {
-		fmt.Println("firstMult")
-		left, _ := strconv.Atoi(line[:firstMult])
-		nextOperation := indexOfFirstOp(line[firstMult+1:])
-		fmt.Println(line[firstMult+1:], "nextOperation", nextOperation)
-		right, _ := strconv.Atoi(line[firstMult:nextOperation])
-		line = calcPrecedenceLine(fmt.Sprint(left+right) + line[:nextOperation])
-		firstMult = indexOfFirst(line, '*')
-	}
-	return line
 }
 
 func calculateLine(line string) (int, int) {
@@ -196,6 +61,127 @@ func calculateLine(line string) (int, int) {
 	}
 	return res, len(line)
 }
+
+// Day 1 --^^
+// Day 2 __vv
+
+func day18sol2(lines []string) int {
+	sum := 0
+	for _, line := range lines {
+		fmt.Println("======================", line)
+		line = strings.ReplaceAll(line, " ", "")
+		fmt.Println("cleanline", line)
+		res := calcBrackets(line)
+		sum += res
+		fmt.Println("======================", res)
+		fmt.Println(sum)
+	}
+	return sum
+
+}
+
+func calcBrackets(line string) int {
+	latestOpeningBracket := indexOfLast(line, '(')
+	for latestOpeningBracket != -1 {
+		fmt.Println("hasBrackets", line)
+		offset := indexOfFirst(line[latestOpeningBracket:], ')')
+		firstClosing := latestOpeningBracket + offset
+
+		fmt.Println("->", line[latestOpeningBracket+1:firstClosing])
+		res := calcPrecedenceLine(line[latestOpeningBracket+1 : firstClosing])
+		line = line[:latestOpeningBracket] + fmt.Sprint(res) + line[firstClosing+1:]
+		fmt.Println("newline", line)
+		latestOpeningBracket = indexOfLast(line, '(')
+	}
+	return calcPrecedenceLine(line)
+}
+
+func indexOfLast(line string, cQuery rune) int {
+	latestOpeningBracket := -1
+	for i, c := range line {
+		if c == cQuery {
+			latestOpeningBracket = i
+		}
+	}
+	return latestOpeningBracket
+}
+func indexOfFirst(line string, cQuery rune) int {
+	for i, c := range line {
+		if c == cQuery {
+			return i
+		}
+	}
+	return -1
+}
+func calcPrecedenceLine(line string) int {
+	fmt.Println("line", line)
+	operations := splitOps(line)
+	hasPlus := hasOp(operations, "+")
+	for hasPlus > 0 {
+		left, _ := strconv.Atoi(operations[hasPlus-1])
+		right, _ := strconv.Atoi(operations[hasPlus+1])
+		res := fmt.Sprint(left + right)
+		operations[hasPlus-1] = res
+		operations = append(operations[:hasPlus], operations[hasPlus+2:]...)
+		fmt.Println("operations", operations)
+		hasPlus = hasOp(operations, "+")
+	}
+	hasMult := hasOp(operations, "*")
+	for hasMult > 0 {
+		left, _ := strconv.Atoi(operations[hasMult-1])
+		right, _ := strconv.Atoi(operations[hasMult+1])
+		res := fmt.Sprint(left * right)
+		operations[hasMult-1] = res
+		operations = append(operations[:hasMult], operations[hasMult+2:]...)
+		fmt.Println("operations", operations)
+		hasMult = hasOp(operations, "*")
+	}
+	fmt.Println(operations)
+	if len(operations) > 1 {
+		fmt.Println("Error errore reerorro")
+		return -1
+	}
+
+	res, _ := strconv.Atoi(operations[0])
+	// res, _ := strconv.Atoi(operations[0])
+	return res
+}
+func splitOps(line string) []string {
+	var mult []string
+	var operations []string
+	toks := strings.Split(line, "*")
+	for i, tok := range toks {
+		mult = append(mult, tok)
+		if i < (len(toks) - 1) {
+			mult = append(mult, "*")
+		}
+	}
+	for _, m := range mult {
+		if strings.Contains(m, "+") {
+			plus := strings.Split(m, "+")
+			for i, p := range plus {
+				operations = append(operations, p)
+				if i < (len(plus) - 1) {
+					operations = append(operations, "+")
+				}
+			}
+		} else {
+			operations = append(operations, m)
+		}
+	}
+	return operations
+}
+
+func hasOp(operations []string, op string) int {
+	for i, o := range operations {
+		if o == op {
+			return i
+		}
+	}
+	return -1
+}
+
+// Day 2 --^^
 
 func day18test() {
 	day18sol1([]string{"1 + 2 + 3"})
