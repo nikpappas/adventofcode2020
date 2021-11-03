4c7d2e91a574fa3d3f5c8be2589bba64974912a7
pappasn-sd <N.Pappas@elsevier.com>
2021-11-03 10:07:45 +0000

    Fix runner script

--- run.sh
+++ run.sh
@@ -2,8 +2,5 @@ RE='^[0-9]+$'
 if ! [[ $1 =~ $RE ]] ; then
    echo "Usage: ./run.sh <Number of day to run>" >&2; exit 1
 else 
-   go run src/*.go $1
+   go run $(ls -1 *.go | grep -v _test.go) $1
 fi
- 
-
-
